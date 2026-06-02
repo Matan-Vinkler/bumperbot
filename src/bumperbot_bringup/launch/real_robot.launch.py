eff566206d9af6599c0b7e8550892b5465eb61b9
Matan Vinkler <matanvinkler@gmail.com>
2026-06-02 16:16:04 +0300

    Added LIDAR, localization and mapping drivers to brinup

--- src/bumperbot_bringup/launch/real_robot.launch.py
+++ src/bumperbot_bringup/launch/real_robot.launch.py
@@ -1,10 +1,18 @@
 import os
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch.conditions import IfCondition, UnlessCondition
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
+    use_slam_arg = DeclareLaunchArgument(
+        "use_slam",
+        default_value="false"
+    )
+    use_slam = LaunchConfiguration("use_slam")
+
     hardware_interface = IncludeLaunchDescription(
         os.path.join(
             get_package_share_directory("bumperbot_firmware"),
@@ -40,8 +48,48 @@ def generate_launch_description():
         executable="mpu6050_driver.py"
     )
 
+    safety_stop = Node(
+        package="bumperbot_utils",
+        executable="safety_stop",
+        output="screen"
+    )
+
+    laser_driver = Node(
+        package="rplidar_ros",
+        executable="rplidar_node",
+        name="rplidar_node",
+        parameters=[
+            os.path.join(get_package_share_directory("bumperbot_bringup"), "config", "rplidar_a1.yaml")
+        ],
+        output="screen"
+    )
+
+    localization = IncludeLaunchDescription(
+        os.path.join(
+            get_package_share_directory("bumperbot_localization"),
+            "launch",
+            "global.launch.py"
+        ),
+        condition=UnlessCondition(use_slam)
+    )
+
+    slam = IncludeLaunchDescription(
+        os.path.join(
+            get_package_share_directory("bumperbot_mapping"),
+            "launch",
+            "slam.launch.py"
+        ),
+        condition=IfCondition(use_slam)
+    )
+
     return LaunchDescription([
+        use_slam_arg,
         hardware_interface,
         controller,
         joystick,
+        imu_driver_node,
+        safety_stop,
+        laser_driver,
+        localization,
+        slam
     ])
